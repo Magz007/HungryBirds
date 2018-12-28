5b30e25022abdc794e0d1b6948fb43eeba94ece2
Alexis Jones <21406612@bucks.ac.uk>
2018-12-28 17:43:52 +0000

    Scaffolded Students page

---
+++ HungryBrids/Data/PupilContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HungryBirds.Models
+{
+    public class PupilContext : DbContext
+    {
+        public PupilContext (DbContextOptions<PupilContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HungryBirds.Models.Student> Student { get; set; }
+    }
+}
